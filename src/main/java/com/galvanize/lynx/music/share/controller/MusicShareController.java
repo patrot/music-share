94e54921b11f1d8227f5169f61320814762c90e0
balajiachanta <balajiachanta@gmail.com>
2021-01-30 10:52:57 -0600

    get all playlists returns 200 (#2)

---
+++ src/main/java/com/galvanize/lynx/music/share/controller/MusicShareController.java
@@ -0,0 +1,15 @@
+package com.galvanize.lynx.music.share.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MusicShareController {
+
+    @GetMapping("/api/v1/musicshare/playlists")
+    public void getAllPlaylists(){
+
+
+    }
+
+}
--- src/test/java/com/galvanize/lynx/music/share/MusicShareApplicationTests.java
+++ src/test/java/com/galvanize/lynx/music/share/MusicShareApplicationTests.java
@@ -1,13 +1,31 @@
 package com.galvanize.lynx.music.share;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MusicShareApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Get All Playlists")
+	public void testGetAll_Returns_Playlist() throws Exception {
+
+		mockMvc.perform(get("/api/v1/musicshare/playlists"))
+				.andExpect(status().isOk());
+
 	}
+
 
 }
