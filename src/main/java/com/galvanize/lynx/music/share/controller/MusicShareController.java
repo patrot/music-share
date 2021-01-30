c4bc1d50bb3ff3626391061617103f3317129f30
tkrishnak <37309528+tkrishnak@users.noreply.github.com>
2021-01-30 14:01:34 -0500

    Feature/get play lists (#6)
    * Added service, repo, entity and modified test case to test empty playList
    
    * Added swagger, Modified entity with annotaions and test case for 1 playList

--- src/main/java/com/galvanize/lynx/music/share/controller/MusicShareController.java
+++ src/main/java/com/galvanize/lynx/music/share/controller/MusicShareController.java
@@ -1,15 +1,22 @@
 package com.galvanize.lynx.music.share.controller;
 
+import com.galvanize.lynx.music.share.model.PlayList;
+import com.galvanize.lynx.music.share.service.MusicShareService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MusicShareController {
 
+    @Autowired
+    private MusicShareService musicShareService;
+
     @GetMapping("/api/v1/musicshare/playlists")
-    public void getAllPlaylists(){
-
-
+    public List<PlayList> getAllPlaylists(){
+        return musicShareService.getAllPlayLists();
     }
 
 }
