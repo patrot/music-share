961ca3abedb1f635c3ece4bdfa7af91b05c3e6c0
tkrishnak <tkrishnagokul@gmail.com>
2021-01-30 13:56:41 -0500

    Added swagger, Modified entity with annotaions and test case for 1 playList

--- src/main/java/com/galvanize/lynx/music/share/model/PlayList.java
+++ src/main/java/com/galvanize/lynx/music/share/model/PlayList.java
@@ -1,17 +1,25 @@
 package com.galvanize.lynx.music.share.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="playlist")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayList {
 
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
 }
