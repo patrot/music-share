4f133a13c6d305875af60ac52e084a01aca7497d
tkrishnak <37309528+tkrishnak@users.noreply.github.com>
2021-01-30 17:00:54 -0500

    Added test to check non exist song, added repo for Song, fixed add song method (#9)

--- src/main/java/com/galvanize/lynx/music/share/controller/ControllerAdvisor.java
+++ src/main/java/com/galvanize/lynx/music/share/controller/ControllerAdvisor.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.galvanize.lynx.music.share.exceptions.ProvidePlaylistNameException;
+import com.galvanize.lynx.music.share.exceptions.SongNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,6 +23,18 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         ObjectNode missingPlayListName = mapper.createObjectNode();
         missingPlayListName.put("message", "Playlist name needed");
         String messageObject = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(missingPlayListName);
+
+        return new ResponseEntity<>(messageObject, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SongNotFoundException.class)
+    public ResponseEntity<Object> handleSongNotFoundException(
+            SongNotFoundException ex, WebRequest request) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode songNotFoundMsg = mapper.createObjectNode();
+        songNotFoundMsg.put("message", "Song doesn't exist");
+        String messageObject = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(songNotFoundMsg);
 
         return new ResponseEntity<>(messageObject, HttpStatus.BAD_REQUEST);
     }
