f23d5c91e35c0bc782c5a55674ec7064a1a3e185
Peter Trotter <patrotter@hotmail.com>
2021-01-30 09:54:23 -0600

    Create music share spring boot application

---
+++ src/main/java/com/galvanize/lynx/music/share/MusicShareApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.lynx.music.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/lynx/music/share/MusicShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.lynx.music.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
