08d16dc82a458415c60c89ce992ad96ef8cf9113
Peter Trotter <patrotter@hotmail.com>
2021-01-30 12:41:45 -0600

    Implement playlists POST when playlist name not provided

--- src/main/java/com/galvanize/lynx/music/share/controller/MusicShareController.java
+++ src/main/java/com/galvanize/lynx/music/share/controller/MusicShareController.java
@@ -1,6 +1,10 @@
 package com.galvanize.lynx.music.share.controller;
 
+import com.galvanize.lynx.music.share.exceptions.ProvidePlaylistNameException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,12 @@ public class MusicShareController {
     public void getAllPlaylists(){
 
 
+    }
+
+    @PostMapping("/api/v1/musicshare/playlist")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void createNewPlaylist() throws ProvidePlaylistNameException {
+        throw new ProvidePlaylistNameException();
     }
 
 }
